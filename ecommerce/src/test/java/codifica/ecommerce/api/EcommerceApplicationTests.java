549abfb8c72ad9b0a1614fa4a37b4083d93db459
Jonathas Rodrigues <jonathasrc96@gmail.com>
2025-08-21 18:52:41 -0300

    feat(Environment): Adicionando configuraçao inicial para executar api com docker-compose

---
+++ ecommerce/src/test/java/codifica/ecommerce/api/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package codifica.ecommerce.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
